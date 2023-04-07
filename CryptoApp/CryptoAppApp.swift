323b8f426b62b49ef517d82cad9399941d5c9e63
DimitrijeVolarevic <dimitrijevolarevic9@gmail.com>
2023-04-07 21:00:46 +0200

    Initial Commit

---
+++ CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by Dimitrije Volarevic on 7.4.23..
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
